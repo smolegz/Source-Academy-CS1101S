921023b82ababe9d5c79a86c2c94c819ce158319
Source Academy User <Ivan Koo>
2022-09-15 14:19:16 +0800

    Changes made from Source Academy

--- R6.js
+++ R6.js
@@ -17,8 +17,15 @@ const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 insertion_sort_cmp(xs, (x, y) => x < y );
 insertion_sort_cmp(xs, (x, y) => x > y );
 insertion_sort_cmp(xs, (x, y) => false );
-insertion_sort_cmp(xs, (x, y) => x % 2 === 0 && x > y && y % 2 === 0? false : y % 2 !== 0 && x % 2 !== 0 && x >= y ? true : x % 2 === 0 && y % 2 !== 0? true : 
-x % 2 === 0 && x === y && y % 2 === 0? true : false);
+insertion_sort_cmp(xs, (x, y) => x % 2 === 0 && x > y && y % 2 === 0
+? false
+: y % 2 !== 0 && x % 2 !== 0 && x >= y
+? true 
+: x % 2 === 0 && y % 2 !== 0
+? true 
+: x === y
+? true 
+: false);
 // Result: list(2, 4, 6, 6, 8,// 9, 7, 5, 3, 1)
 
 //2a) O(n)
