d9b16d97a6551fc80ea1d09da8a9ef189403019d
Ivan Koo <No public email provided>
2022-10-10 01:49:18 +0800

    Changes made from Source Academy

---
+++ studio9.js
@@ -0,0 +1,19 @@
+//q1
+// still 0;
+
+//q2
+function d_filter(pred, xs) {
+    if (is_null(xs)) {
+        return list();
+    } else if (pred(head(xs))) {
+         set_tail(xs, d_filter(pred,tail(xs)));
+         return xs;
+    } else {
+        return d_filter(pred, tail(xs));
+    }
+}
+
+const L =  list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+
+display(d_filter(x => x % 2 === 0, L)); 
+L;
