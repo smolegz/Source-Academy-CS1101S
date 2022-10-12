d8c5291382d09017361bec4ed26196c589bd4d33
Ivan Koo <No public email provided>
2022-10-12 18:58:32 +0800

    Changes made from Source Academy

---
+++ ev3traceline.js
@@ -0,0 +1,71 @@
+const right = ev3_motorB();
+const left = ev3_motorC();
+const gyro = ev3_gyroSensor();
+const color_sens = ev3_colorSensor();
+
+function waitRun(motor, time, speed) {
+    ev3_runForTime(motor, 1000 * time, speed);
+    ev3_pause(time * 1000);
+}
+
+function move(right_wheel, left_wheel, pos, speed) {
+    ev3_runToRelativePosition(right_wheel, pos, speed);
+    ev3_runToRelativePosition(left_wheel, pos, speed);
+    ev3_pause(1000 * pos / speed);
+}
+
+function traceline() {
+    let on_track = true;
+    let run = true;
+    let k = 0;
+    let speed = 60; 
+    
+    while (run) {
+        
+        while(on_track) {
+            k = 0;
+            move(right, left, 10, 100)
+            on_track = ev3_reflectedLightIntensity(color_sens) <= 10;
+        }
+        
+        while (!on_track) {
+            if (k === 20) {
+                run = false;
+                break;
+            } else {}
+            display("not on track. Iteration: " + stringify(k));
+            
+            waitRun(right, 10, speed) // turn LEFT and check for line
+            on_track = (ev3_reflectedLightIntensity(color_sens)) <= 10;
+            if (on_track) {
+                break;
+            } else {
+                waitRun(left, 10, speed) // turn back original position
+                waitRun(left, 10, speed) //  turn right and check for line
+                on_track = (ev3_reflectedLightIntensity(color_sens)) <= 10;
+                if (on_track) {
+                    break;
+                } else {
+                    waitRun(right, 10, speed); //turn back original position
+                }
+            }
+            speed = speed + 20;
+            k = k + 1;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
