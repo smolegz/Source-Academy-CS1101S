c683ab5310710db1df6d00ebb5137d054ae16166
Source Academy User <Ivan Koo>
2022-10-13 14:53:58 +0800

    Changes made from Source Academy

--- R9.js
+++ R9.js
@@ -1,14 +1,33 @@
-function make_optimized_search(A) {
+// function make_optimized_search(A) {
 
-    const B = [];
-    let i = 0;
+//     const B = [];
+//     let i = 0;
     
-    while (array_length(B) !== array_length(A)) {
-        B[i] = A[i];
-        i = i + 1;
+//     while (array_length(B) !== array_length(A)) {
+//         B[i] = A[i];
+//         i = i + 1;
+//     }
+//     merge_sort(B); // not declared;
+    
+//     return x => binarysearch(B,x); // not declared;
+// }
+
+function fib(n) {
+    let fib0 = 0;
+    let fib1 = 1;
+    let result = 0;
+    
+    for (let i = 2; i <= n; i = i + 1) {
+        result = fib0 + fib1;
+        fib0 = fib1;
+        fib1 = result;
     }
-    merge_sort(B); // not declared;
     
-    return x => binarysearch(B,x); // not declared;
+    return result;
 }
 
+fib(1);
+
+//q3
+length = k;
+Theta((n-k)k);
