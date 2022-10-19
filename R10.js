a832b8beb1e5e094d79ac635dcff8c7c5d14f9e4
Source Academy User <Ivan Koo>
2022-10-20 01:35:29 +0800

    Changes made from Source Academy

--- R10.js
+++ R10.js
@@ -1,27 +1,54 @@
-function zip_list_of_streams(xs) {
-    const n = length(xs);
-    function stream(ys, n) {
-        if (n === 0) {
+// function zip_list_of_streams(xs) {
+//     const n = length(xs);
+//     function stream(ys, n) {
+//         if (n === 0) {
             
-            return zip_list_of_streams(map(stream_tail, xs));
-        }
-        return pair(head(head(ys)), () => stream(tail(ys), n -1));
+//             return zip_list_of_streams(map(stream_tail, xs));
+//         }
+//         return pair(head(head(ys)), () => stream(tail(ys), n -1));
+//     }
+
+//     return stream(xs,n);
+// }
+
+// const ones = pair(1, () => ones);
+// const twos = pair(2, () => twos);
+// const threes = pair(3, () => threes);
+
+// let x= zip_list_of_streams(list(ones,twos,threes));
+// eval_stream(x, 10);
+
+function add_streams(s1, s2) {
+    if (is_null(s1)) {
+        return s2;
+    } else if (is_null(s2)) {
+        return s1;
+    } else {
+        return pair(head(s1) + head(s2),
+                () => add_streams(stream_tail(s1),
+                        stream_tail(s2)));
     }
-
-
-
-
-    return stream(xs,n);
-
-
-
-    
 }
 
-const ones = pair(1, () => ones);
-const twos = pair(2, () => twos);
-const threes = pair(3, () => threes);
+const from2 = integers_from(2);
+function partial_sums(s) {
+    return pair(1, () => add_streams(partial_sums(from2), from2));
+}
 
-let x= zip_list_of_streams(list(ones,twos,threes));
-eval_stream(x, 10);
+const sum = partial_sums(from2);
+
+stream_ref(sum, 4);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
