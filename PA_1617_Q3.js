1d3b766d291e01808baf3e4f406acb6f5e731e95
Ivan Koo <No public email provided>
2022-11-08 21:36:44 +0800

    Changes made from Source Academy

---
+++ PA_1617_Q3.js
@@ -0,0 +1,416 @@
+// Instructions for students who are using this for practice:
+//
+// (1) Copy and paste this entire file into the editor of Source Academy
+//     Playground at https://sourceacademy.nus.edu.sg/playground
+// (2) Write your solution for each task in the Source Academy Playground.
+// (3) Run the program to test your solution on the given testcases.
+
+
+////////////////////////////////////////////////////////////
+// Question 3A
+////////////////////////////////////////////////////////////
+
+function evaluate_BAE_tree(bae_tree) {
+    if (is_number(bae_tree)) {
+        return bae_tree;
+    } else {
+        const op = head(tail(bae_tree));
+
+        const left = evaluate_BAE_tree(head(bae_tree));
+    
+        const right = evaluate_BAE_tree(head(tail(tail(bae_tree))));
+    
+        if (op === "+") {
+            return left + right;
+        } else if (op === "-") {
+            return left - right;
+        } else if (op === "*") {
+            return left * right;
+        } else {
+            return left / right;
+        }
+    }
+
+}
+
+
+
+////////////////////////////////////////////////////////////
+// Question 3B
+////////////////////////////////////////////////////////////
+
+function build_BAE_tree(bae_list) {
+    // WRITE HERE.
+    let next_token = bae_list;
+
+    function build_tree() {
+        if (equal(head(next_token), "(")) {
+            next_token = tail(next_token);
+            const left_tree = build_tree();
+            const op = head(next_token);
+            next_token = tail(next_token);
+            const right_tree = build_tree();
+            next_token = tail(next_token); // skip over ")"
+            return list(left_tree, op, right_tree);
+        } else { // token is a number
+            const token = head(next_token);
+            next_token = tail(next_token);
+            return token;
+        }
+    }
+
+    return build_tree();
+}
+
+
+
+////////////////////////////////////////////////////////////
+// Question 3C
+////////////////////////////////////////////////////////////
+
+
+function evaluate_BAE(bae_list) {
+
+    // WRITE HERE.
+
+}
+
+
+
+////////////////////////////////////////////////////////////
+// Question 3D
+////////////////////////////////////////////////////////////
+
+function check_parentheses(paren_list) {
+    if (is_null(paren_list)) {
+        return true;
+    }
+    if (head(paren_list) === ")") {
+        return false;
+    }
+    let open = 0;
+    
+    function helper(paren_list) {
+        if (open < 0) {
+            return false;
+        }
+        if (is_null(paren_list)) {
+            return open;
+        }
+        if (head(paren_list) === "(") {
+            open = open + 1;
+            return helper(tail(paren_list));
+        }
+        if (head(paren_list) === ")") {
+            open = open - 1;
+            return helper(tail(paren_list));
+        }
+    }
+    helper(paren_list);
+    
+    return open === 0;
+}
+
+
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+
+//===========================================================
+// This function is provided for running the testcases
+// in the Source Academy Playground.
+// They are NOT part of the actual testing facility provided
+// in the actual Practical Assessment.
+//===========================================================
+function assert(f, test_name, fnames) {
+    display(test_name + ": " + (f() ? "PASS" : "FAIL"));
+}
+//===========================================================
+
+
+
+////////////////////////////////////////////////////////////
+// Test Cases for Q3A
+////////////////////////////////////////////////////////////
+
+assert(
+    () => {
+        const bae_tree = 23;
+        return equal(evaluate_BAE_tree(bae_tree), 23);
+    },
+    "Q3A-T1",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(5, "*", 6);
+        return equal(evaluate_BAE_tree(bae_tree), 30);
+    },
+    "Q3A-T2",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(5, "*", list(7, "+", 3));
+        return equal(evaluate_BAE_tree(bae_tree), 50);
+    },
+    "Q3A-T3",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(8, "-", 2), "*", list(7, "+", 3));
+        return equal(evaluate_BAE_tree(bae_tree), 60);
+    },
+    "Q3A-T4",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(list(20, "/", 2), "-", 2), "*",
+                            list(7, "+", 3));
+        return equal(evaluate_BAE_tree(bae_tree), 80);
+    },
+    "Q3A-T5",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = 100;
+        return equal(evaluate_BAE_tree(bae_tree), 100);
+    },
+    "Q3A-T6",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(70, "-", 15);
+        return equal(evaluate_BAE_tree(bae_tree), 55);
+    },
+    "Q3A-T7",
+    ['evaluate_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(list(7, "+", 5), "*", 3), "/",
+                            list(list(20, "/", 2), "-", list(100, "-", 94)));
+        return equal(evaluate_BAE_tree(bae_tree), 9);
+    },
+    "Q3A-T8",
+    ['evaluate_BAE_tree']
+);
+
+
+
+////////////////////////////////////////////////////////////
+// Test Cases for Q3B
+////////////////////////////////////////////////////////////
+
+assert(
+    () => {
+        const bae_tree = 23;
+        const bae_list = list(23);
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T1",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(5, "*", 6);
+        const bae_list = list("(", 5, "*", 6, ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T2",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(5, "*", list(7, "+", 3));
+        const bae_list = list("(", 5, "*", "(", 7, "+", 3, ")", ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T3",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(8, "-", 2), "*", list(7, "+", 3));
+        const bae_list = list("(", "(", 8, "-", 2, ")", "*",
+                            "(", 7, "+", 3, ")", ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T4",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(list(20, "/", 2), "-", 2), "*",
+                            list(7, "+", 3));
+        const bae_list = list("(", "(", "(", 20, "/", 2, ")", "-", 2, ")", "*",
+                            "(", 7, "+", 3, ")", ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T5",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = 100;
+        const bae_list = list(100);
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T6",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(70, "-", 15);
+        const bae_list = list("(", 70, "-", 15, ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T7",
+    ['build_BAE_tree']
+);
+
+assert(
+    () => {
+        const bae_tree = list(list(list(7, "+", 5), "*", 3), "/",
+                            list(list(20, "/", 2), "-", list(100, "-", 94)));
+        const bae_list = list("(", "(", "(", 7, "+", 5, ")", "*", 3, ")", "/",
+                            "(", "(", 20, "/", 2, ")", "-",
+                                 "(", 100, "-", 94, ")", ")", ")");
+        return equal(build_BAE_tree(bae_list), bae_tree);
+    },
+    "Q3B-T8",
+    ['build_BAE_tree']
+);
+
+
+
+////////////////////////////////////////////////////////////
+// Test Cases for Q3C
+////////////////////////////////////////////////////////////
+
+assert(
+    () => {
+        const bae_list = list(23);
+        return equal(evaluate_BAE(bae_list), 23);
+    },
+    "Q3C-T1",
+    ['evaluate_BAE']
+);
+
+assert(
+    () => {
+        const bae_list = list("(", 5, "*", 6, ")");
+        return equal(evaluate_BAE(bae_list), 30);
+    },
+    "Q3C-T2",
+    ['evaluate_BAE']
+);
+
+assert(
+    () => {
+        const bae_list = list("(", "(", "(", 20, "/", 2, ")", "-", 2, ")", "*",
+                            "(", 7, "+", 3, ")", ")");
+        return equal(evaluate_BAE(bae_list), 80);
+    },
+    "Q3C-T3",
+    ['evaluate_BAE']
+);
+
+
+
+////////////////////////////////////////////////////////////
+// Test Cases for Q3D
+////////////////////////////////////////////////////////////
+
+assert(
+    () => {
+        const paren_list = list();
+        return equal(check_parentheses(paren_list), true);
+    },
+    "Q3D-T1",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", ")");
+        return equal(check_parentheses(paren_list), true);
+    },
+    "Q3D-T2",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", "(", "(", ")", ")",
+                                   "(", "(", ")", "(", ")", ")", ")");
+        return equal(check_parentheses(paren_list), true);
+    },
+    "Q3D-T3",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list(")", "(");
+        return equal(check_parentheses(paren_list), false);
+    },
+    "Q3D-T4",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", "(", ")", ")", ")", "(", "(", ")");
+        return equal(check_parentheses(paren_list), false);
+    },
+    "Q3D-T5",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", "(", ")", "(");
+        return equal(check_parentheses(paren_list), false);
+    },
+    "Q3D-T6",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", ")", "(", ")", "(", ")");
+        return equal(check_parentheses(paren_list), true);
+    },
+    "Q3D-T7",
+    ['check_parentheses']
+);
+
+assert(
+    () => {
+        const paren_list = list("(", "(", "(", ")", ")",
+                                   "(", "(", ")", ")", ")", ")", ")");
+        return equal(check_parentheses(paren_list), false);
+    },
+    "Q3D-T8",
+    ['check_parentheses']
+);
